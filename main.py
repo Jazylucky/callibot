7499c04ace1dd957afb40cd4b26bc6933cec1e22
Jazylucky <81566910+Jazylucky@users.noreply.github.com>
2022-05-10 16:20:45 +0200

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -4,16 +4,33 @@ def on_forever():
     pass
 basic.forever(on_forever)
 
-def check_line_l():
+def tick():
+    check_line()
+
+
+
+def check_abs_vorne():
+    if callibot.entfernung(KEinheit.CM) <= 20:
+        
+
+def check_line():
     if callibot.read_line_sensor(KSensor.LINKS, KSensorStatus.DUNKEL):
         rotate("right")
         callibot.motor(KMotor.BEIDE, KDir.VORWÄRTS, 100)
         basic.pause(200)
         rotate("left")
-
+    if callibot.read_line_sensor(KSensor.RECHTS, KSensorStatus.DUNKEL):
+        rotate("left")
+        callibot.motor(KMotor.BEIDE, KDir.VORWÄRTS, 100)
+        basic.pause(200)
+        rotate("right")
 
 def rotate(direction):
     if direction == "right":
         callibot.motor(KMotor.LINKS, KDir.VORWÄRTS, 50)
+        basic.pause(500)
+        callibot.motor_stop(KMotor.BEIDE, KStop.BREMSEN)
     elif direction == "left":
         callibot.motor(KMotor.RECHTS, KDir.VORWÄRTS, 50)
+        basic.pause(500)
+        callibot.motor_stop(KMotor.BEIDE, KStop.BREMSEN)
--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 basic.forever(function on_forever() {
     
 })
-function check_line_l() {
+function tick() {
+    check_line()
+}
+
+function check_line() {
     if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel)) {
         rotate("right")
         callibot.motor(KMotor.beide, KDir.vorwärts, 100)
@@ -9,13 +13,24 @@ function check_line_l() {
         rotate("left")
     }
     
+    if (callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
+        rotate("left")
+        callibot.motor(KMotor.beide, KDir.vorwärts, 100)
+        basic.pause(200)
+        rotate("right")
+    }
+    
 }
 
 function rotate(direction: string) {
     if (direction == "right") {
         callibot.motor(KMotor.links, KDir.vorwärts, 50)
+        basic.pause(500)
+        callibot.motorStop(KMotor.beide, KStop.Bremsen)
     } else if (direction == "left") {
         callibot.motor(KMotor.rechts, KDir.vorwärts, 50)
+        basic.pause(500)
+        callibot.motorStop(KMotor.beide, KStop.Bremsen)
     }
     
 }
